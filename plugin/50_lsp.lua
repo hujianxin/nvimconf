eada02ed34705519220eb329ddcbacb22e2f481c
为霖 <weilin.hjx@antgroup.com>
2026-04-21 10:09:03 +0800

    Add kulala.nvim and nvim-nio plugins, consolidate lspconfig/mason add call

--- plugin/50_lsp.lua
+++ plugin/50_lsp.lua
@@ -6,8 +6,10 @@ local add = vim.pack.add
 local now_if_args, later = Config.now_if_args, Config.later
 
 now_if_args(function()
-  add({ "https://github.com/neovim/nvim-lspconfig" })
-  add({ "https://github.com/williamboman/mason.nvim" })
+  add({
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/williamboman/mason.nvim",
+  })
   require("mason").setup({
     ui = {
       border = "rounded",
